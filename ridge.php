c92e7038108c24c9ec542e8868b4e80a322b39d9
bogan <GeorgeWBush1>
2017-04-25 12:56:38 +1000

    Added files found on web server to VCS

---
+++ ridge.php
@@ -0,0 +1,33 @@
+			<?php $title = 'Ridge';	?>
+			<?php include 'header.php'; ?>
+
+			<div>
+				<h2>Location</h2>
+		
+				<p>Cooleman Ridge is situated between Chapman 
+				and Kambah lying north-west to south-east, about 3.5 km long with an area 
+				of 187 ha. Its highest point is Mt Arawang (765 m) with a general level 
+				of 600 m above sea level. It offers extensive views east and north over 
+				Canberra and west over the Brindabella mountain range.</p>
+				
+				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13015.342693021319!2d149.03646483522206!3d-35.359724246198!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sau!4v1444473478793" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+		
+				<h2>Recent History</h2>
+		
+				<p>Cooleman Ridge has been grazed by sheep 
+				and cattle for over a hundred years and consequently has many exotic grasses 
+				and weeds. As part of 'Yarralumla' property, 1899 maps indicate open forest. 
+				Latterly it was part of the 'Cooleman' grazing property when it was sown 
+				with <i>Phalaris</i> pasture grass and fertilised. It has been part of Canberra 
+				Nature Park formally since 1993 but was a reserve before that.</p>
+		
+				<h2>Geology</h2>
+		
+				<p>Did you know that Cooleman Ridge, like 
+				most of Canberra, sits on rocks 420 million years old? Volcanic activity, 
+				the ocean, lakes and weathering have all played their part in forming today's 
+				landscape. Read Dr. John F. Lindsay's interesting <a href="geology.php">
+				article</a> for more information about the geology of the area.</p>
+			<div>
+			
+			<?php include 'footer.php'; ?>
---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+ini_set("log_errors", 1);
+ini_set("error_log", "php-error.log");
+error_log( "Hello, errors!" );
+echo("Hello, world!");
+?>

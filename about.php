5dc124385b20432072ab92795f8afea9eaeca848
Rohan Thomas (Home) <rohanthomas@bigpond.com>
2017-04-29 14:00:57 +1000

    Updated about and news pages

--- about.php
+++ about.php
@@ -1,51 +1,61 @@
 			<?php $title = 'About';	?>
 			<?php include 'header.php'; ?>
 			<div>
-				<h2>About the Group</h2>
+				<h3>About the Group</h3>
 				
 				<p>
-				Cooleman Ridge Park Care Group Inc. is one of a number 
-				of park care groups around Canberra which take an interest in our Nature 
-				Parks. Thanks to the work of members, weeds are kept down, tracks are maintained 
-				and native trees, bushes and grasses are planted.
-				</p>
-					
-				<p>The group was formed in 1991. Since then it has removed the majority 
-				of the woody weeds <i>Pyracantha, Rosa rubiginosa</i> (Sweet Briar), etc, 
-				and greatly reduced thistles and <i>Verbascum.</i> Large areas of <i>Phalaris</i> 
-				grass have been cut to reduce fire fuel. Native tree plantations have been 
-				established and mid-storey plants and native grasses put in to many areas. 
-				Only indigenous trees and plants are used.</p>
-				
-				<h2>Activities</h2>
-				
-				<p>
-					Members typically have their own small area to caretake 
-					and also cooperate to work over the whole area at monthly meetings. This 
-					enables us to have an overall strategy and help each other on the larger 
-					jobs.
-				</p>
-				<p>
-					The group meets once a month on the third Sunday. Our
-					<a href="news.php">newsletter</a> gives details. Smaller groups meet weekly 
-					on Monday and Friday mornings.
-				</p>
-				<p>
-					At our meetings we:
-				</p>
-				<p>
+				The Cooleman Ridge Park Care Group is a <a href="http://www.environment.act.gov.au/parks-conservation/parks-and-reserves/get-involved/parkcare-and-urban-landcare-program">ParkCare</a> group that &quot;cares&quot; for the Cooleman Ridge Nature Reserve.
+                </p>
+
+                <h3>Mission</h3>
+
+                <p>
+                The mission of the Cooleman Ridge Park Care Group is to &quot;restore the bush&quot; by returning the ecology of the nature reserve to it's pre-European state.
+                </p>
+
+
+
+                <h3>History</h3>
+
+				<p>The group was formed in 1991 by three ladies - Bess Sledge, Pauline Lynga and Tina Nevins.
+
+                    The group has now grown to have 50 registered members that contribute in various ways with small but active core of members that regulary meet and work on the nature reserve.
+
+<!--                    Since then it has removed the majority -->
+<!--				of the woody weeds <i>Pyracantha, Rosa rubiginosa</i> (Sweet Briar), etc, -->
+<!--				and greatly reduced thistles and <i>Verbascum.</i> Large areas of <i>Phalaris</i> -->
+<!--				grass have been cut to reduce fire fuel. Native tree plantations have been -->
+<!--				established and mid-storey plants and native grasses put in to many areas. -->
+<!--				Only indigenous trees and plants are used.-->
+
+                </p>
+
+
+                <h3>Meetings</h3>
+
+                <p>
+				The group meets once a month on the third Sunday of every month for a group working party. The group works on one of designated group work areas.
+                Please see our latest <a href="news.php">newsletter</a> for the specific details for our next meeting.
+                </p>
+
+                <p>
+                There are also smaller weekly work groups that focus on just one specific area of the nature reserve. These include:
+                <ul>
+                    <li>Kathner Street Group - every Friday morning at the Kathner Street Dam site</li>
+                    <li>Arawang Group - every Wednesday morning at the Mt Arawang site</li>
+                </ul>
+
+                </p>
+
+
+                <h3>Work</h3>
+
+                <p>The group performs a wide variety of work including:
 					<ul>
 						<li>Weed </li>
 						<li>Plant </li>
 						<li>Study the wildlife </li>
 						<li>Take part in theme walks</li>
-					</ul>
-				</p>
-				<p>
-					Members are also involved with:
-				</p>
-				<p>
-					<ul>
 						<li>Tree survey </li>
 						<li>Bird monitoring </li>
 						<li>Fire fuel reduction </li>
@@ -55,8 +65,34 @@
 						<li>Photographic record </li>
 					</ul>
 				</p>
-				<p>
-					To find out more about our activities please email <a href="mailto:admin@coolemanridge.org.au">Cooleman Ridge Parkcare Group</a>.
-				</p>
+
+<!--                <p>-->
+<!--                    Thanks to the work of members, weeds are kept down, tracks are maintained-->
+<!--                    and native trees, bushes and grasses are planted.-->
+<!---->
+<!--                    Members typically have their own small area to caretake-->
+<!--                    and also cooperate to work over the whole area at monthly meetings. This-->
+<!--                    enables us to have an overall strategy and help each other on the larger-->
+<!--                    jobs.-->
+<!--                </p>-->
+
+
+
+                <h3>Partners</h3>
+
+                <p>
+                    The group works with a number of other organisations: These include:
+                <ul>
+                    <li>Green Army</li>
+                    <li>ACT Government</li>
+                    <li>Southern ACT Catchement Group</li>
+                </ul>
+                </p>
+
+                <h3>Contact</h3>
+
+                <p>
+                    To find out more about our activities please email <a href="mailto:admin@coolemanridge.org.au">Cooleman Ridge Parkcare Group</a>.
+                </p>
 			</div>
 			<?php include 'footer.php'; ?>

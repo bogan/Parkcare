9dda529f04c4bd3691e5bbc5b1a66e520b1decc3
Rohan Thomas (Home) <rohanthomas@bigpond.com>
2017-04-30 09:15:31 +1000

    More reformatting of HTML of the landing page prior to refactor

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <!-- saved from url=(0039)index.html -->
-<HTML>
-<HEAD>
-    <TITLE>Cooleman Ridge Park Care Group</TITLE>
-    <META content="text/html; charset=iso-8859-1" http-equiv=Content-Type>
-    <META name=title content="Cooleman Ridge Park Care Group">
-    <META name=DC:title content="Cooleman Ridge Park Care Group">
-    <META name=keywords content="environment, nature park, nature parks, cooleman ridge, parkcare, park care, weeds, walks, bicycle riding, bike riding, horse riding, fire, fires, January 2003,">
-    <META name=DC:subject content="environment, nature park, nature parks, cooleman ridge, parkcare, park care, weeds, walks, bicycle riding, bike riding, horse riding, fire, fires, January 2003,">
-    <META name=description content="Cooleman Ridge is situated between Chapman and Kambah lying north-west to south-east, about 3.5 km long with an area of 187 ha.  Its highest point is Mt Arawang (765 m) with a general level of 600 m above the Canberra Plain.  It offers extensive views east and north over Canberra and west over the Brindabella mountain range.">
-    <META name=DC:description content="Cooleman Ridge is situated between Chapman and Kambah lying north-west to south-east, about 3.5 km long with an area of 187 ha.  Its highest point is Mt Arawang (765 m) with a general level of 600 m above the Canberra Plain.  It offers extensive views east and north over Canberra and west over the Brindabella mountain range.">
-    <META name=Author content="Karina Pelling">
-    <META name=Keywords content="environment, nature park, nature parks, cooleman ridge, parkcare, park care, weeds, walks, bicycle riding, bike riding, horse riding, fire, fires, January 2003">
-    <META name=GENERATOR content="MSHTML 9.00.8112.16443">
+<html>
+<head>
+    <title>Cooleman Ridge Park Care Group</title>
+    <meta content="text/html; charset=iso-8859-1" http-equiv=Content-Type>
+    <meta name=title content="Cooleman Ridge Park Care Group">
+    <meta name=DC:title content="Cooleman Ridge Park Care Group">
+    <meta name=keywords content="environment, nature park, nature parks, cooleman ridge, parkcare, park care, weeds, walks, bicycle riding, bike riding, horse riding, fire, fires, January 2003,">
+    <meta name=DC:subject content="environment, nature park, nature parks, cooleman ridge, parkcare, park care, weeds, walks, bicycle riding, bike riding, horse riding, fire, fires, January 2003,">
+    <meta name=description content="Cooleman Ridge is situated between Chapman and Kambah lying north-west to south-east, about 3.5 km long with an area of 187 ha.  Its highest point is Mt Arawang (765 m) with a general level of 600 m above the Canberra Plain.  It offers extensive views east and north over Canberra and west over the Brindabella mountain range.">
+    <meta name=DC:description content="Cooleman Ridge is situated between Chapman and Kambah lying north-west to south-east, about 3.5 km long with an area of 187 ha.  Its highest point is Mt Arawang (765 m) with a general level of 600 m above the Canberra Plain.  It offers extensive views east and north over Canberra and west over the Brindabella mountain range.">
+    <meta name=Author content="Karina Pelling">
+    <meta name=Keywords content="environment, nature park, nature parks, cooleman ridge, parkcare, park care, weeds, walks, bicycle riding, bike riding, horse riding, fire, fires, January 2003">
+    <meta name=GENERATOR content="MSHTML 9.00.8112.16443">
     <link href="/images/favicon.ico" rel="shortcut icon" type="image/x-icon"/>
-</HEAD>
-<BODY>
-    <STYLE>TD {
+</head>
+<body>
+    <style>
+    TD {
         FONT-FAMILY: "Arial"
     }
     A:link {
@@ -34,12 +35,12 @@
     IMG {
         BORDER-BOTTOM-COLOR: black; BORDER-TOP-COLOR: black; BORDER-RIGHT-COLOR: black; BORDER-LEFT-COLOR: black
     }
-    </STYLE>
+    </style>
 
-    <TABLE border=0 cellSpacing=0 cellPadding=0 width=800 background=images/silhcen5g.png align=center height=600>
-        <TBODY>
-          <TR>
-            <TD height=20 vAlign=middle colSpan=6 align=center>&nbsp;</TD><td colSpan=3><div>
+    <table border=0 cellSpacing=0 cellPadding=0 width=800 background=images/silhcen5g.png align=center height=600>
+        <tbody>
+          <tr>
+            <td height=20 vAlign=middle colSpan=6 align=center>&nbsp;</td><td colSpan=3><div>
                 <script>
                   (function() {
                     var cx = '008804451297429151083:h_lh6xzgjre';
@@ -54,98 +55,95 @@
                 </script>
                 <gcse:search></gcse:search></div>
             </td>
-          </TR>
-          <TR>
-            <TD height=70 vAlign=middle width=90 align=center><IMG
-              src="images/logo1.jpg" width=54 height=50></TD>
-            <TD height=70 vAlign=middle colSpan=7 align=center><FONT color=#ba7025
-              size=+3 face="Arial, Helvetica, sans-serif">Cooleman Ridge Park Care
-              Group</FONT></TD>
-            <TD height=70 vAlign=middle width=90 align=left>&nbsp;</TD>
-          </TR>
-          <TR>
-            <TD height=20 width=90>&nbsp;</TD>
-            <TD width=140>&nbsp;</TD>
-            <TD>&nbsp;</TD>
-            <TD>&nbsp;</TD>
-            <TD>&nbsp;</TD>
-            <TD>&nbsp;</TD>
-            <TD>&nbsp;</TD>
-            <TD width=140>&nbsp;</TD>
-            <TD width=90>&nbsp;</TD>
-          </TR>
-          <TR>
-            <TD height=132 vAlign=middle align=center>&nbsp;</TD>
-            <TD height=132 vAlign=middle width=140 align=center><A href="about.php"><IMG border=1 alt="" align=middle src="images/fernembl.jpg" width=130 height=130></A></TD>
-            <TD vAlign=middle width=20 align=center>&nbsp;</TD>
-            <TD height=132 vAlign=middle width=140 align=center><A href="place.php"><IMG border=1 alt="" align=middle src="images/treesnow.jpg" width=130 height=130></A></TD>
-            <TD vAlign=middle width=20 align=center>&nbsp;</TD>
-            <TD height=132 vAlign=middle width=140 align=center><A href="geology.php"><IMG border=1 alt="" align=middle src="images/rocksnow.jpg" width=130 height=130></A></TD>
-            <TD vAlign=middle width=20 align=center>&nbsp;</TD>
-            <TD height=132 vAlign=middle width=140 align=center><A href="flora.php"><IMG border=1 align=middle src="images/hibertic.jpg" width=130 height=130></A></TD>
-            <TD vAlign=middle align=center>&nbsp;</TD>
-          </TR>
-          <TR>
-            <TD height=40 vAlign=middle align=center>&nbsp;</TD>
-            <TD height=40 vAlign=middle align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif"><A href="about.php">The Group</A></FONT></TD>
-            <TD vAlign=middle width=20 align=center>&nbsp;</TD>
-            <TD vAlign=middle align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif"><A href="place.php">Place</A></FONT></TD>
-            <TD vAlign=middle width=20 align=center>&nbsp;</TD>
-            <TD vAlign=middle align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif"><A href="geology.php">Earth</A> </FONT></TD>
-            <TD vAlign=middle width=20 align=center>&nbsp;</TD>
-            <TD vAlign=middle align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif"><A href="flora.php">Flora</A></FONT></TD>
-            <TD vAlign=middle align=center>&nbsp;</TD>
-          </TR>
-          <TR>
-            <TD height=20 vAlign=middle colSpan=2 align=center>&nbsp;</TD>
-            <TD vAlign=middle align=center>&nbsp;</TD>
-            <TD vAlign=middle align=center>&nbsp;</TD>
-            <TD vAlign=middle width=20 align=center>&nbsp;</TD>
-            <TD vAlign=middle align=center>&nbsp;</TD>
-            <TD vAlign=middle width=20 align=center>&nbsp;</TD>
-            <TD vAlign=middle align=center>&nbsp;</TD>
-            <TD vAlign=middle align=center>&nbsp;</TD>
-          </TR>
-          <TR>
-            <TD height=132 vAlign=middle align=center>&nbsp;</TD>
-            <TD height=132 vAlign=middle width=140 align=center><A href="members.php"><IMG border=1 align=middle src="images/doug.jpg" width=130 height=130></A></TD>
-            <TD vAlign=middle width=20 align=center>&nbsp;</TD>
-            <TD height=132 vAlign=middle width=140 align=center><A href="news.php"><IMG border=1 align=middle src="images/benchdam.jpg" width=130 height=130></A></TD>
-            <TD vAlign=middle width=20 align=center>&nbsp;</TD>
-            <TD height=132 vAlign=middle width=140 align=center><A href="walks.php"><IMG border=1 align=middle src="images/damwalks.jpg" width=130 height=130></A></TD>
-            <TD height=132 vAlign=middle width=20 align=center>&nbsp;</TD>
-            <TD height=132 vAlign=middle width=140 align=center><A href="fauna.php"><IMG border=1 align=middle src="images/kestrel.jpg" width=130 height=130></A></TD>
-            <TD vAlign=middle align=center>&nbsp;</TD>
-          </TR>
-          <TR>
-            <TD height=40 vAlign=middle align=center>&nbsp;</TD>
-            <TD height=40 vAlign=middle align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif"><A href="members.php">Members</A></FONT></TD>
-            <TD vAlign=middle width=20 align=center>&nbsp;</TD>
-            <TD vAlign=middle align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif"><A href="news.php">News</A></FONT></TD>
-            <TD vAlign=middle width=20 align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif">&nbsp;</FONT></TD>
-            <TD vAlign=middle align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif"><A href="walks.php">Walks, Links</A></FONT></TD>
-            <TD vAlign=middle width=20 align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif">&nbsp;</FONT></TD>
-            <TD vAlign=middle width=140 align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif"><A href="fauna.php">Fauna</A></FONT></TD>
-            <TD vAlign=middle align=center>&nbsp;</TD>
-          </TR>
-          <TR>
-            <TD height=40 vAlign=middle colSpan=9 align=center><FACE="ARIAL, sans-serif? Helvetica,><A href="news/Chapmanbrolores.pdf">Brochure</A>
-              -:- <A href="news/draft_plan.pdf">1997 DRAFT MANAGEMENT PLAN</FONT></A> -:- <A href="news/RegenReport.pdf">Regeneration Report</A></TD>
-          </TR>
-          <TR>
-            <TD height=86 vAlign=middle colSpan=9 align=center>&nbsp;</TD>
-          </TR>
-        </TBODY>
-    </TABLE>
-    <TABLE border=0 cellSpacing=0 cellPadding=10 width=800 align=center height=126>
-        <TBODY>
-            <TR>
-                <TD height=20 vAlign=middle colSpan=9 align=center>&nbsp;</TD>
-            </TR>
-            <TR>
-                <TD height=86 vAlign=middle colSpan=9 align=center>&nbsp;</TD>
-            </TR>
-        </TBODY>
-    </TABLE>
-    </BODY>
-</HTML>
+          </tr>
+          <tr>
+            <td height=70 vAlign=middle width=90 align=center><IMG src="images/logo1.jpg" width=54 height=50></td>
+            <td height=70 vAlign=middle colSpan=7 align=center><FONT color=#ba7025 size=+3 face="Arial, Helvetica, sans-serif">Cooleman Ridge Park Care Group</FONT></td>
+            <td height=70 vAlign=middle width=90 align=left>&nbsp;</td>
+          </tr>
+          <tr>
+            <td height=20 width=90>&nbsp;</td>
+            <td width=140>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td width=140>&nbsp;</td>
+            <td width=90>&nbsp;</td>
+          </tr>
+          <tr>
+            <td height=132 vAlign=middle align=center>&nbsp;</td>
+            <td height=132 vAlign=middle width=140 align=center><A href="about.php"><img border=1 alt="" align=middle src="images/fernembl.jpg" width=130 height=130></A></td>
+            <td vAlign=middle width=20 align=center>&nbsp;</td>
+            <td height=132 vAlign=middle width=140 align=center><A href="place.php"><IMG border=1 alt="" align=middle src="images/treesnow.jpg" width=130 height=130></A></td>
+            <td vAlign=middle width=20 align=center>&nbsp;</td>
+            <td height=132 vAlign=middle width=140 align=center><A href="geology.php"><IMG border=1 alt="" align=middle src="images/rocksnow.jpg" width=130 height=130></A></td>
+            <td vAlign=middle width=20 align=center>&nbsp;</td>
+            <td height=132 vAlign=middle width=140 align=center><A href="flora.php"><IMG border=1 align=middle src="images/hibertic.jpg" width=130 height=130></A></td>
+            <td vAlign=middle align=center>&nbsp;</td>
+          </tr>
+          <tr>
+            <td height=40 vAlign=middle align=center>&nbsp;</td>
+            <td height=40 vAlign=middle align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif"><A href="about.php">The Group</A></FONT></td>
+            <td vAlign=middle width=20 align=center>&nbsp;</td>
+            <td vAlign=middle align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif"><A href="place.php">Place</A></FONT></td>
+            <td vAlign=middle width=20 align=center>&nbsp;</td>
+            <td vAlign=middle align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif"><A href="geology.php">Earth</A> </FONT></td>
+            <td vAlign=middle width=20 align=center>&nbsp;</td>
+            <td vAlign=middle align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif"><A href="flora.php">Flora</A></FONT></td>
+            <td vAlign=middle align=center>&nbsp;</td>
+          </tr>
+          <tr>
+            <td height=20 vAlign=middle colSpan=2 align=center>&nbsp;</td>
+            <td vAlign=middle align=center>&nbsp;</td>
+            <td vAlign=middle align=center>&nbsp;</td>
+            <td vAlign=middle width=20 align=center>&nbsp;</td>
+            <td vAlign=middle align=center>&nbsp;</td>
+            <td vAlign=middle width=20 align=center>&nbsp;</td>
+            <td vAlign=middle align=center>&nbsp;</td>
+            <td vAlign=middle align=center>&nbsp;</td>
+          </tr>
+          <tr>
+            <td height=132 vAlign=middle align=center>&nbsp;</td>
+            <td height=132 vAlign=middle width=140 align=center><A href="members.php"><IMG border=1 align=middle src="images/doug.jpg" width=130 height=130></A></td>
+            <td vAlign=middle width=20 align=center>&nbsp;</td>
+            <td height=132 vAlign=middle width=140 align=center><A href="news.php"><IMG border=1 align=middle src="images/benchdam.jpg" width=130 height=130></A></td>
+            <td vAlign=middle width=20 align=center>&nbsp;</td>
+            <td height=132 vAlign=middle width=140 align=center><A href="walks.php"><IMG border=1 align=middle src="images/damwalks.jpg" width=130 height=130></A></td>
+            <td height=132 vAlign=middle width=20 align=center>&nbsp;</td>
+            <td height=132 vAlign=middle width=140 align=center><A href="fauna.php"><IMG border=1 align=middle src="images/kestrel.jpg" width=130 height=130></A></td>
+            <td vAlign=middle align=center>&nbsp;</td>
+          </tr>
+          <tr>
+            <td height=40 vAlign=middle align=center>&nbsp;</td>
+            <td height=40 vAlign=middle align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif"><A href="members.php">Members</A></FONT></td>
+            <td vAlign=middle width=20 align=center>&nbsp;</td>
+            <td vAlign=middle align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif"><A href="news.php">News</A></FONT></td>
+            <td vAlign=middle width=20 align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif">&nbsp;</FONT></td>
+            <td vAlign=middle align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif"><A href="walks.php">Walks, Links</A></FONT></td>
+            <td vAlign=middle width=20 align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif">&nbsp;</FONT></td>
+            <td vAlign=middle width=140 align=center><FONT color=#0000ff face="Arial, Helvetica, sans-serif"><A href="fauna.php">Fauna</A></FONT></td>
+            <td vAlign=middle align=center>&nbsp;</td>
+          </tr>
+          <tr>
+            <td height=40 vAlign=middle colSpan=9 align=center><FACE="ARIAL, sans-serif? Helvetica,><A href="news/Chapmanbrolores.pdf">Brochure</A>
+              -:- <A href="news/draft_plan.pdf">1997 DRAFT MANAGEMENT PLAN</FONT></A> -:- <A href="news/RegenReport.pdf">Regeneration Report</A></td>
+          </tr>
+          <tr>
+            <td height=86 vAlign=middle colSpan=9 align=center>&nbsp;</td>
+          </tr>
+        </tbody>
+    </table>
+    <table border=0 cellSpacing=0 cellPadding=10 width=800 align=center height=126>
+        <tbody>
+            <tr>
+                <td height=20 vAlign=middle colSpan=9 align=center>&nbsp;</td>
+            </tr>
+            <tr>
+                <td height=86 vAlign=middle colSpan=9 align=center>&nbsp;</td>
+            </tr>
+        </tbody>
+    </table>
+    </body>
+</html>

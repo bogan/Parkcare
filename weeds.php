da148ad45b33914f623bdbb0b692df275f767bca
bogan <rohanthomas@bigpond.com>
2017-05-09 16:04:21 +1000

    Updated content of weeds page

--- weeds.php
+++ weeds.php
@@ -10,18 +10,172 @@
 
     <h2 class="page-header">Weeds</h2>
 
-    <p>Most inexperienced people often confuse natives and exotics with one another.</p>
 
-    <p>There are many different weeds on the ridge but there are probably a dozen or so main weeds that the group works on.</p>
 
-    <p>This page is designed to give somebody new to the ridge a gentle introduction to these signficant weeds.</p>
+    <p>Weeds and weeding are the main focus of the group. There are large number of different types of weeds on the ridge. However, there are roughly a dozen weed species that constitute the main bulk of the weeds on the ridge.</p>
 
 
 
-    <p>The first step in helping to solve the problem is to identify the problem. You can help by simply being aware what are weeds and what are not.</p>
 
-    <p>The following is a shortened list of the main weeds that the Group focuses on. For a complete list of all weeds found on the Cooleman Ridge Nature Reserve, please see our <a href="https://en.wikipedia.org/wiki/Bush_regeneration#Bradley_method">Plant Survey</a>.</p>
+    <p>The group believes that it is important that everybody be able to recognise the weeds, even if they do not actively weed themselves. The first step in helping to solve the problem is to identify the problem. You can help by simply being aware what are weeds and what are not. Most inexperienced people often confuse natives and exotics with one another. </p>
 
+    <p>This page is here to give somebody new to the ridge a gentle introduction to its most signficant weeds.  Each species is quite easy to identify, even for a novice. The following is a shortened list of the main weeds that the Group focuses on. For a complete list of all weeds found on the Cooleman Ridge Nature Reserve, please see our <a href="https://en.wikipedia.org/wiki/Bush_regeneration#Bradley_method">Plant Survey</a>.</p>
+
+    <div class="panel panel-info">
+        <div class="panel-heading">Blackberry <em>(Rubus fruticosus)</em></div>
+        <div class="panel-body">
+
+            <img src="images/blackberry.jpg" style="height:100px;width:100px;float:left;padding:0px;margin: 5px"/>
+
+            <p>The Cooleman Ridge has a major blackberry problem, especially on the eastern face of Mt Arawang.  The group tracks the position of every known blackberry bush as part of its <a href="blackberrys.php">Blackberry Survey</a>.</p>
+
+            <p>Blackberry is only effectively treated with poisoning. However, it is effectively beyond the capability of the group to control some of the the larger infestations. These locations are then communicated to Parks and Conservation workers/contractors who use inustrial standard spraying equipment. Blackberrys are typcially sprayed in late summer or autumn when they are actively growing and after  fruiting.</p>
+
+            <p> N.B. Blackberry is easily confused with the Native Rasberry because they look very similar and are from the same genus.</p>
+        </div>
+    </div>
+
+    <div class="panel panel-info">
+        <div class="panel-heading">Phalaris <em>(Phalaris aquatica)</em></div>
+        <div class="panel-body">
+
+            <img src="images/phalaris.jpg" style="height:100px;width:100px;float:left;padding:0px;margin: 5px"/>
+
+            <p>
+                Phalaris is a large, thick, perenial tussock grass. In mass plantings, it forms thick mats that dominate all other plants. It was deliberately introduced pasture grass by former pastoralist to signficantly increase the amount of feed available for their grazing animals.
+            </p>
+
+            <p>
+                Phalaris is also significant because it is the major fuel source and can rapidly increase the speed and intersity of grass fires. Eliminating pharlaris is important part of minimising dangerous grassfires on the ridge. See our <a href="fire.php"> Fire</a> page for more information.
+            </p>
+
+
+            <p>
+                This weed is widespread on the ridge. The most significant examples are found more commonly found on the south west facing slopes of the ridge such as between Lincoln Place and Arawang Homestead. It is typically found in water courses.
+            </p>
+
+            <p>
+                It is not very invasive and can be cnntrolled either by digging it out or by poisoning. The group has made significant progress with removing pharlaris from the <a href="groupareas.php">Group Areas</a>.</p>
+            </p>
+
+        </div>
+    </div>
+
+    <div class="panel panel-info">
+        <div class="panel-heading">Great Mullien <em>(Verbascum thapsus)</em></div>
+        <div class="panel-body">
+
+            <img src="images/verbascum.jpg" style="height:100px;width:100px;float:left;padding:0px;margin: 5px"/>
+
+            <p>Great Mullien is easy to control and responds well to hacking or poisoning. The group has been able to largely eliminate this weed in each of the group areas.</p>
+
+        </div>
+    </div>
+
+    <div class="panel panel-info">
+        <div class="panel-heading">Wild Sage <em>(Salvia verbenaca)</em></div>
+        <div class="panel-body">
+
+            <img src="images/wildsage.jpg" style="height:100px;width:100px;float:left;padding:0px;margin: 5px"/>
+
+            <p>Wild Sage is a common herbaceous weed that is closely related to culniary sage. It is fairly widespread on the ridge. It is quite easy to control and responds well to hacking or poisoning. The group has been able to largely eliminate this weed in each of the group areas.</p>
+
+        </div>
+    </div>
+
+    <div class="panel panel-info">
+        <div class="panel-heading">St Johns Wort <em>(Hypericum perforatum)</em></div>
+        <div class="panel-body">
+
+            <img src="images/stjohnswort.jpg" style="height:100px;width:100px;float:left;padding:0px;margin: 5px"/>
+
+            <p>Johns wort is highly invasive. It is one of the few weeds that will invade non damaged errors. There is currently little effective control of this weed.  It is very common on the wetter and cooler south western slopes of the ridge.</p>
+
+        </div>
+    </div>
+
+    <div class="panel panel-info">
+        <div class="panel-heading">African Lovegrass <em>(Eragrostis curvula)</em></div>
+        <div class="panel-body">
+
+            <img src="images/africanlovegrass.jpg" style="height:100px;width:100px;float:left;padding:0px;margin: 5px"/>
+
+            <p>Is a highly invasive weed and . The ridge has some significant patches of lovegrass. The most signficant example is found around the large reservoir above Darrell Place.
+                Lovegrass is reasonably easy to control and can be hacked out or poisoned.
+            </p>
+
+        </div>
+    </div>
+
+    <div class="panel panel-info">
+        <div class="panel-heading">Patterson's Curse <em>(Echium plantagineum)</em></div>
+        <div class="panel-body">
+
+            <img src="images/pattersonscurse.jpg" style="height:100px;width:100px;float:left;padding:0px;margin: 5px"/>
+
+            <p>Patterson's Curse is a small to medium sized herbaceous weed. It has bright purple leaves and is very fast growing. It takes off in wet periods at any time of the year, rapidly flowers and seeds, before quickly dying off in dry periods.            </p>
+
+            <p>The ridge has had large infestations of Paterson's Curse in the past especailly on the western end of the ridge  highly invasive weed and . The ridge has some significant patches of Paterson's Curse. The most signficant example is found around the Kathner Street Damlarge reservoir above Darrell Place.
+                Lovegrass is reasonably easy to control and can be hacked out or poisoned.
+            </p>
+
+        </div>
+    </div>
+
+    <div class="panel panel-info">
+        <div class="panel-heading">Bearded Oats <em>(Avena barbata)</em></div>
+        <div class="panel-body">
+
+            <img src="images/wildoats.jpg" style="height:100px;width:100px;float:left;padding:0px;margin: 5px"/>
+
+            <p>
+                Bearded Oats is a tall, slender and  rapidly growing grass. It is annual that germinates late winter and rapidly grows to maturity within a few weeks. It is a common weed in former pastured areas.
+            </p>
+
+            <p>
+                This weed is common throughout the ridge and is typically found occurs largely in damaged areas or areas that were heavily grazed. It is most common on the western ends of the ridge around Kathner Street and Chauvel Circuit.
+            </p>
+
+            <p>
+                Wild Oats is difficult to directly control because both of its speed of growth and the fact that it does not have a rosette that allows hacking or spraying. However it does not compete very well with native grass areas and can easily be indirectly controlled by replacing infested areas with native grasses. which will quickly out competed.
+            </p>
+        </div>
+    </div>
+
+    <div class="panel panel-info">
+        <div class="panel-heading">Sweet Briar <em>(Rosa rubiginosa)</em> </div>
+        <div class="panel-body">
+
+            <img src="images/briarrose.jpg" style="height:100px;width:100px;float:left;padding:0px;margin: 5px"/>
+
+            <p>Briar Rose is the most significant woody weed on the reserve. It has large red/orange berrys on it and is easily spread by birds.   The group tracks the position of every known briar rose bush as part of its <a href="woodyweeds.php">Wood Weed Survey</a>. The group has now largely eliminated this weed from the ridge but pockets still remain.
+            </p>
+
+        </div>
+    </div>
+
+    <div class="panel panel-info">
+        <div class="panel-heading">Fleabane  <em>(Conyza  bonariensis )</em></div>
+        <div class="panel-body">
+
+            <img src="images/fleabane.jpg" style="height:100px;width:100px;float:left;padding:0px;margin: 5px"/>
+
+            <p>Fleabane is a large slender herbaceous weed. It is most common in wet seasons. Fleabane is reasonably easy to control and can be hacked out or poisoned. The group has largely controlled this weed on the ridge.
+            </p>
+
+        </div>
+    </div>
+
+    <div class="panel panel-info">
+        <div class="panel-heading">Scotch Thistle <em>(Onopordum  acanthium )</em></div>
+        <div class="panel-body">
+
+            <img src="images/scotchthistle.jpg" style="height:100px;width:100px;float:left;padding:0px;margin: 5px"/>
+
+            <p>Scotch Thistle has been a major problem on the ridge. This was especially the case on the summit Mt Arawang. It can easily be controlled with either hacking or poisoning. The group has largely eliminated this weed from the ridge.</p>
+
+        </div>
+    </div>
 
 
 <?php include 'footer.php'; ?>

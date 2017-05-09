ae7d53ca497187c45b9fe4ab7fbad65e9427d3bb
Rohan Thomas (Laptop) <rohan.thomas@bigpond.com>
2017-05-09 20:06:45 +1000

    Added more content to the trails and activities pages

--- activities.php
+++ activities.php
@@ -16,17 +16,21 @@
 
     <img src="images/doug.png" style="height:100px;width:100px;float:left;padding:5px;margin: 5px;border:none "/>
 
-    <p>Weeding is the main day to day activity. Although weeding is not glamourous, the group believes that it is the single most effective activity that can be used to restore the ecology of the ridge.</p>
+    <p>Weeding is the main day to day activity of the group. Although weeding is not glamourous, the group believes that it is the single most effective activity that can be used to restore the ecology of the ridge.</p>
 
-    <p>Our belief in the effectiveness of weeding is based on our own experience and the experience of similar landcare groups. Please see our <a href="methods.php">methods page</a> for a more detailed explanation of how weeding is a vital part of our strategy to regenerate the native vegetation of the ridge.</p>
+    <p>Our belief in the effectiveness of weeding is based on our own experience with weeding and the experience of similar landcare groups. Please see our <a href="methods.php">methods page</a> for a more detailed explanation of how weeding is a vital part of our strategy to regenerate the native vegetation of the ridge.</p>
+
+    <p>A well planned and executed weeding stategy can have a major impact on th regeneration of the landscape.</p>
 
     <h3>Planting</h3>
 
     <img src="images/potting.jpg" style="height:100px;width:100px;float:left;padding:5px;margin: 5px;border:none "/>
 
-    <p>The group either plants new plantings or assists other groups (eg. the Green Army) in the planting of new trees, shrubs and grasses. The nature reserve now has some significant plantations. Please visit our plantations page for more details.</p>
+    <p>The group sometimes participates in plantings of farious native grasses, forbs, shrubs and trees. These blants are either source from seedlings grown offsite or by transplating small plants from one part of the ridge to another. Plantings are also performed on the ridge either by other groups (eg. the Green Army) in the planting of new trees, shrubs and grasses. The nature reserve now has some significant plantations. Please visit our plantations page for more details.</p>
 
-    <p>Blah, blah, blah</p>
+    <p>Many of the plants are now very mature and provide many additional benefits for the ridge such as weed suppresion, windbreaks, and habitati for the animals on the ridge.</p>
+
+    <p>The group performed many mass plantings in its early years. The focus of the group has steadily shifted away from planting to weeding partly because most areas have now been planted out but partly because the group believes that weeding is amore cost effective approach.</p>
 
     <h3>Surveys</h3>
 
@@ -34,7 +38,7 @@
 
     <p>The group undertakes various surveys that record a significant aspect of the nature reserve (eg. plants, animals, weeds, etc). These surveys are used to assist in the ongoing management of the ridge. Please visit our <a href="surveys.php">surveys</a> page for more information.</p>
 
-<p>They also are an important way to record the history of the ridge and the groups work on it for future generations.</p>
+    <p>They also are an important way to record the history of the ridge and the groups work on it for future generations.</p>
 
     <p>Blah, blah, blah</p>
 
--- trails.php
+++ trails.php
@@ -11,41 +11,65 @@
 
     <p>The Cooleman Ridge Nature Reserve is lucky to be home to a wide variety of tracks that are of local, regional and national significance.</p>
 
-    <p>These various trails serve a variety of purposes ranging from recreation to management.</p>
+    <p>These various trails serve a variety of purposes ranging from recreation (eg. walking, cycling, horse riding) to management (eg. weed spraying, fire control, etc).</p>
 
-    <p>These trails play an important role for connecting the work of the group with the broader community.</p>
+    <p>These trails play an important role for connecting the work of the group with the broader community through their recreational.</p>
 
-    <h3>Bicentennial National Trail (BNP)</h3>
+    <h3>Bicentennial National Trail (BNT)</h3>
 
-    <p>The <a href="www.bicentennialnationaltrail.com.au">Bicentennial National Trail</a> is a 5000km multipurpose trail that runs from Healesville Victoria to Cooktown Queensland.
-    Part of this trail crosses the Cooleman Ridge Nature Reserve. The trail is mainly used by equestrian riders but people have been known to travel using other forms of transport such as feet, donkeys and even camels!
+    <p>The <a href="www.bicentennialnationaltrail.com.au">Bicentennial National Trail</a> is a 5000km multipurpose trail that runs from Healesville Victoria to Cooktown Queensland. It is Australia's longest formally recognised track and one of the longest in the world.
+        Part of this trail crosses the Cooleman Ridge Nature Reserve along its souther and western boundaries.
     </p>
 
-    <p>The Bicentennial National Trail is the only part of the nature reserve that horses are allowed on.</p>
+    <p>
+        The BCT is essentally a recreational trail and is used by people travelling its full length or just part of it. The trail is mainly used by equestrian riders but people have been known to travel using other forms of transport such as feet, donkeys and even camels!
+    </p>
 
+    <p>
+        The Bicentennial National Trail is the only part of the nature reserve that horses are allowed on.</p>
     </p>
 
     <h3>Canberra Centenary Trail</h3>
 
-    <p>The <a href="http://www.environment.act.gov.au/parks-conservation/parks-and-reserves/find-a-park/rural/canberra-centenary-trail">Canberra Centenary Trail</a> is an ACT based multipurpose track that circumnavigates the city of Canberra.</p>
+    <p>
+        The <a href="http://www.environment.act.gov.au/parks-conservation/parks-and-reserves/find-a-park/rural/canberra-centenary-trail">Canberra Centenary Trail</a> is an ACT based multipurpose track that circumnavigates the city of Canberra. The track was created in 2013 to celebrate the 100th anniversery of the founding of the city of Canberra and is intended for use by both walkers and bike rider.
+    </p>
 
-    <p>The track was created in 2013 to celebrate the 100th anniversery of the founding of the city of Canberra.</p>
+    <p>
+        The section that cross the nature reserve is composed of both existing management vehicular trails as well as specially contructed walking trails.
+    </p>
 
-    <p>The section that cross the nature reserve is composed of both existing management vehicular trails as well as specially contrusted walking trails.</p>
+    <p>
+        The section that crosses the Cooleman Ridge crosses all of our group work areas offers some of the best views that can be seen along the trail, looking out over the Murrumbidge River valley and beyond to the Bullen, Tidbinbilla and Brindabella ranges.
+    </p>
 
-    <p>The section that crosses the Cooleman Ridge offers some of the best views that can be seen along the trail, looking out over the Murrumbidge River valley and beyond to the Bullen, Tidbinbilla and Brindabella ranges.</p>
-
-    <h3>Cooleman Trig Walking Trail</h3>
-
-    <p>To be completed <a href="http://www.environment.act.gov.au/__data/assets/pdf_file/0009/906372/Cooleman-Ridge-Nature-Trail.pdf">Cooleman Ridge Nature Trail</a></p>
+    <p>
+        The CCT is very popular with Canberrans and many. The group tries to maximise the benefit of this by publicing the work that it does through signage.
+    </p>
 
     <h3>Mt Arawang Walking Trail</h3>
 
-    <p>This walking trail allows walkers to summit. The track was upgraded in 2016 and provides good access to the summit. Access is via either Lincoln Place or Namatijira Drive.</p>
+    <p>
+        The Mt Arawang Walking Trail is a walking trail allows walkers to access the summit of Mt Arawang, the highest point on the ridge. The trail has two entry/exit points - one at Namitjira and the other at Lincoln Place.</p>
+    </p>
+
+    <p>
+        The track was upgraded in 2016 and provides a well formed path to the summit. The views from the summit are some of the best from Canberra. The return walk is approximately 1 hour return.
+    </p>
+
+    <h3>Cooleman Trig Walking Trail</h3>
+
+    <p>
+        This trail provides access to the summit of Cooleman Trig - the second highest peak on the ridge. The track is short but steep and is composed off a well formed sealed path with steps. Access is from Monkman Street. allow half an hour for the return walk.
+    </p>
 
     <h3>Cooleman Ridge Nature Trail</h3>
 
-    <p>To be completed <a href="http://www.environment.act.gov.au/__data/assets/pdf_file/0009/906372/Cooleman-Ridge-Nature-Trail.pdf">Cooleman Ridge Nature Trail</a></p>
+    <p>The Cooleman Ridge Nature Trail is walking trail that was developed by the group. The purpose of the track is to introduce the public to some of the natural highlights of the ridge. The trail is marked with numbered posts that identify points of interest that are explained in an accompanying brochure that the group developed.
+
+        The brochure is available online or in the brochure box at the start of the walk.
+
+        The trail is accessed from either Blah Place and is composed of both pupose built waking tracks and preexistin management trails. The walk is acircuit and you should all 1 hourto complete it. To be completed <a href="http://www.environment.act.gov.au/__data/assets/pdf_file/0009/906372/Cooleman-Ridge-Nature-Trail.pdf">Cooleman Ridge Nature Trail</a></p>
 
     <h3>Fire Trails</h3>
 
